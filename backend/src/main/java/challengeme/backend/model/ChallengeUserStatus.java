845e37650d553625617bbc18051ce36c3aaf5fb5
Iustinache <iustin.bivolaru@stud.ubbcluj.ro>
2025-11-04 21:54:10 +0200

    Describe your changes

--- backend/src/main/java/challengeme/backend/model/ChallengeUserStatus.java
+++ backend/src/main/java/challengeme/backend/model/ChallengeUserStatus.java
@@ -1,7 +1,7 @@
 package challengeme.backend.model;
 
 public enum ChallengeUserStatus {
-    PENDING,  // Provocarea a fost văzută, dar nu acceptată încă (sau e default)
-    ACCEPTED, // Utilizatorul a acceptat provocarea
+    PENDING,
+    ACCEPTED,
     COMPLETED
 }
